fa19f948d32a5f6be0e2d66798e3b5b5cce5c7ec
yassesh <yassin2780@gmail.com>
2022-10-03 05:43:48 +0600

    web3 imported

---
+++ coupon_dapp/coupon_frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Web3 from "web3";
+import configuration from "./Coupons.json";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
---
+++ coupon_dapp/coupon_frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-800">
+      <nav className="2xl:container 2xl:mx-auto sm:py-6 sm:px-7 py-5 px-4">
+        <h1 className="font-extrabold font-sans text-teal-400 text-5xl text-center">
+          FINXTER PREMIUM
+        </h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
